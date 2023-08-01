24d0698d64bf420ef149794b7856f9a59b2ca505
Nithish Gowda <nithish.gowda@signify.com>
2023-08-02 00:04:57 +0530

    Merge changes from feature2 branch
    - add contact button in main page, indexTemplate

--- web/templates.go
+++ web/templates.go
@@ -4,18 +4,20 @@ const indexTemplate = `
 <!DOCTYPE html>
 <html>
 <head>
-	<title>{{.Title}}</title>
+    <title>{{.Title}}</title>
 </head>
 <body>
-	<form action="/" method="post">
-		<label for="first_name">First Name:</label>
-		<input type="text" name="first_name" required>
-		<br>
-		<label for="last_name">Last Name:</label>
-		<input type="text" name="last_name" required>
-		<br>
-		<input type="submit" value="Submit">
-	</form>
+    <form action="/" method="post">
+        <label for="first_name">First Name:</label>
+        <input type="text" name="first_name" required>
+        <br>
+        <label for="last_name">Last Name:</label>
+        <input type="text" name="last_name" required>
+        <br>
+        <input type="submit" value="Submit">
+    </form>
+    <br>
+    <a href="/contact">Contact Us</a>
 </body>
 </html>
 `
