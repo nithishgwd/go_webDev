7215b12801275d2994a4a71926a1b4df2a80b44d
Nithish Gowda <nithish.gowda@signify.com>
2023-08-01 23:24:01 +0530

    Added html template to contact us
    user needs to provide his/her name, e-mail and message
    - which can be reviwed laterdded html template to contact us

--- web/templates.go
+++ web/templates.go
@@ -31,3 +31,30 @@ const welcomeTemplate = `
 </body>
 </html>
 `
+
+// added contact template
+const contactTemplate = `
+<!DOCTYPE html>
+<html>
+<head>
+    <title>{{.Title}}</title>
+</head>
+<body>
+    <h2>Contact Us</h2>
+    <form action="/contact" method="post">
+        <label for="name">Name:</label>
+        <input type="text" name="name" required>
+        <br>
+        <label for="email">Email:</label>
+        <input type="email" name="email" required>
+        <br>
+        <label for="message">Message:</label>
+        <textarea name="message" rows="4" required></textarea>
+        <br>
+        <input type="submit" value="Send">
+    </form>
+    <br>
+    <a href="/">Back to Home</a>
+</body>
+</html>
+`
